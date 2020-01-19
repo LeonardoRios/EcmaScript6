9d1b71f16526f9f469f7d0afc1ae4bdad133a071
Leonardo Rios <leonardorios.22@hotmail.com>
2020-01-18 21:06:56 -0300

    exemplo de uso de class com html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("teste");
+//alert("teste");
 
 /* Criando uma class para que o bundle converta em um padrão antigo caso o navegador não conheça */
 class Teste{
@@ -12,3 +12,23 @@ class Teste{
     }
     
 } 
+
+
+class TodoList{
+
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push('Novo todo');
+        console.log(this.todos);
+    }
+
+}
+
+const minhaLista = new TodoList();
+
+document.getElementById("novoTodo").onclick = function(){
+    minhaLista.addTodo();
+}
